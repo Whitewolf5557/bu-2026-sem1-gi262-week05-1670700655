0a868ea58c1e9bf4a8be2cfb6ae0ec1f51cbf158
Whitewolf5557 <onnicha.piti@bumail.net>
2026-09-15 16:25:11 +0700

    .

--- Assets/Workshop/Student/Scripts/OOPPlayer.cs
+++ Assets/Workshop/Student/Scripts/OOPPlayer.cs
@@ -86,7 +86,7 @@ namespace Solution
         {
             var enemies = mapGenerator.GetEnemies();
             //stundent exercise: sort enemies by remainning energy
-
+            Array.Sort(enemies, (a, b) => a.energy.CompareTo(b.energy));
             return enemies;
         }
         public void Attack(OOPEnemy _enemy)
